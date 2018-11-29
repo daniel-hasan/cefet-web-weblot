00baa905a9b0ea0a94289204f6a9b23e88bafc69
Lucas Corrêa <lucashuati@hotmail.com>
2018-11-29 11:57:07 -0200

    Fix notification

--- apis/push/sw2.js
+++ apis/push/sw2.js
@@ -46,9 +46,9 @@ self.addEventListener('push', function(event) {
   console.log('[Service Worker] Push Received.');
   console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
 
-  const title = 'Push Codelab';
+  const title = 'Entrada do vortex!!!';
   const options = {
-    body: 'Yay it works.',
+    body: 'Venha e volte logo.',
     icon: 'images/icon.png',
     badge: 'images/badge.png'
   };
